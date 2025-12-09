0b5a0cc89d4e6e49de268e3c4952476ebccdb045
Cefz Zendejas <molanddise@gmail.com>
2025-12-09 09:31:02 -0600

    The About page was imporvedin terms of styles to have a better presentation

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,15 +2,16 @@ const About = () => {
     const view = `
         <section class="about__container" id="about">
 
-            <div class="container__img">
+            <div class="wrapper">
                 <img src="/assets/Cesar.png" alt="My photo">
             </div>
 
-            <div class="about-text">
+            <div class="wrapper__text">
                 <h2>About<span>Me</span></h2>
                 <h4>Full Stack Developer!</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, officia natus ipsum provident debitis porro accusantium praesentium expedita eligendi nisi id iste. Sunt inventore cupiditate voluptates accusantium odit pariatur maxime!
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, <br/> officia  natus ipsum provident debitis porro accusantium praesentium <br/>expedita eligendi  nisi id iste. Sunt inventore cupiditate voluptates accusantium odit pariatur maxime!
                 </p>
+
                 <a href="#" class="btn-box">More About Me</a>
             </div>
 
