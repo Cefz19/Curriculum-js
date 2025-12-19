b5b41ba2501988f136b02dcb88010c438a7227c7
Cefz Zendejas <molanddise@gmail.com>
2025-12-18 18:13:54 -0600

    New was responsive in part screen Service

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,32 +1,29 @@
 const Services = () => {
     const view = `
         <section class="services__main">
-
-            <div class="container">
                 
-                <div class="title">
-                    <h1 class="sub-title">My <span>Services</span></h1>
-                </div>
+            <div class="title">
+                <h1 class="sub-title">My <span>Services</span></h1>
+            </div>
 
-                <div class="card__list">
-                    <div class="card">
-                        <i class='bx bx-code' style='color:rgba(36,220,237,0.74)'></i>
-                        <h2>UI/UX Desing</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste provident dolorem eaque alias pariatur?</p>
-                        <a href="#" class="read">Learm More</a>
-                    </div>
-                    <div class="card">
-                        <i class='bx bx-code' style='color:rgba(36,220,237,0.74)'></i>
-                        <h2>UI/UX Desing</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste provident dolorem eaque alias pariatur?</p>
-                        <a href="#" class="read">Learm More</a>
-                    </div>
-                    <div class="card">
-                        <i class='bx bx-code' style='color:rgba(36,220,237,0.74)'></i>
-                        <h2>UI/UX Desing</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste provident dolorem eaque alias pariatur?</p>
-                        <a href="#" class="read">Learm More</a>
-                    </div>
+            <div class="card__list">
+                <div class="card">
+                    <i class='bx bx-code' style='color:rgba(36,220,237,0.74)'></i>
+                    <h2>UI/UX Desing</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste provident dolorem eaque alias pariatur?</p>
+                    <a href="#" class="read">Learm More</a>
+                </div>
+                <div class="card">
+                    <i class='bx bx-code' style='color:rgba(36,220,237,0.74)'></i>
+                    <h2>UI/UX Desing</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste provident dolorem eaque alias pariatur?</p>
+                    <a href="#" class="read">Learm More</a>
+                </div>
+                <div class="card">
+                    <i class='bx bx-code' style='color:rgba(36,220,237,0.74)'></i>
+                    <h2>UI/UX Desing</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste provident dolorem eaque alias pariatur?</p>
+                    <a href="#" class="read">Learm More</a>
                 </div>
             </div>
 
