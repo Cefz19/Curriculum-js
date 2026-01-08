5ffcceb2f317ed7f773ffcaaca7e8f9ef3d3e2ac
Cefz Zendejas <molanddise@gmail.com>
2026-01-07 23:02:51 -0600

    The struture was separated for PDF docuemnts so they can be downloded from the DOM

---
+++ src/pages/docs/PorfolioPDF.js
@@ -0,0 +1,12 @@
+const PorfolioPDF = () => {
+    return `
+        <section class="container">
+            <button class="btn btn-primary" id="download">Download PDF</button>
+            <div class="card"> 
+                <iframe src="./SAT.pdf" width="650px" height="800px"></iframe> 
+            </div>
+        </section>
+    `;
+}
+
+export default PorfolioPDF;
