5fbcc373e3d1687c45d213e044b34d5d70c3904b
Cefz Zendejas <molanddise@gmail.com>
2026-01-20 23:04:50 -0600

    New deploy that changed configuration in name .pdf

--- src/data/docsData/PorfolioData.js
+++ src/data/docsData/PorfolioData.js
@@ -2,6 +2,6 @@ export const PorfolioDataPDF = [
     {
         atitle: 'Go back to Home',
         title: 'Download PDF',
-        src: '../docs/CV Ing_Sist_Com.pdf',
+        src: '../docs/cv-ing-sist-com.pdf',
     },
 ];
