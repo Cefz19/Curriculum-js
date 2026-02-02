344676d7d841f920a0cdda0025cff46c92a3b717
Cefz Zendejas <molanddise@gmail.com>
2026-02-01 22:06:05 -0600

    Chages was in title in first card of Modelado de Calzado

--- src/data/moleculesData/ProjectData.js
+++ src/data/moleculesData/ProjectData.js
@@ -4,15 +4,15 @@ export const ProjectsData = [
   {
     src: images[1].src,
     alt: images[1].src,
-    title: "UI/UX Desing",
+    title: "Diseño | Modelado de Calzado",
     description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde excepturi autem, eius ratione est a tempore ullam ab iusto vitae ipsam, libero deserunt non fuga ex recusandae hic commodi?",
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
     icon: "bx-link-external",
   },
   {
     src: images[0].src,
     alt: images[0].src,
-    title: "UI/UX Desing",
+    title: "UI/UX Desin",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque unde excepturi autem, eius ratione est a tempore ullam ab iusto vitae ipsam, libero deserunt non fuga ex recusandae hic commodi?",
     icon: "bx-link-external",
