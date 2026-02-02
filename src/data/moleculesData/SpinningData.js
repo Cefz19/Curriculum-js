5d79f8a927a124a7557095089a33d7dc7ddde426
Cefz Zendejas <molanddise@gmail.com>
2026-02-01 21:48:12 -0600

    The description of the show pattern maker was detiled with the service I offer.

--- src/data/moleculesData/SpinningData.js
+++ src/data/moleculesData/SpinningData.js
@@ -3,8 +3,8 @@ import NeoImg from '../../assets/imgs/Neo.png';
 export const  ImgSpinningData = [
     {
         src: NeoImg,
-        title: 'UX/IX',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum possimus architecto necessitatibus,',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum possimus ',
+        title: 'Modelado de Calzado',
+        description: 'Especialista en Modelaje y Desarrollo Tecnico de Calzado, con dominio integral desde la conceptualizacion de bocetos hasta producccion en serie. Con experincia en patronaje digital(Romance 2D) y diferentes contrucciones, en focado en optimizar la ergonomia mediante el desarrollo preciso de horma y la digitalizacion de procesos para garantiza productos copetitivos y listos para el mercado',
+        details: 'Detalles',
     }
 ]
