3fbaed4c30ab9a50d9197a5c26633ab7c531fc04
Cefz Zendejas <molanddise@gmail.com>
2026-02-11 22:21:59 -0600

    New Images was added in card Modelista

--- src/data/moleculesData/SpinningData.js
+++ src/data/moleculesData/SpinningData.js
@@ -1,16 +1,28 @@
-import NeoImg from '../../assets/imgs/Neo.png';
-import Img1 from '../../assets/imgs/img1.jpeg';
+import ZaptillaImg from '../../assets/imgs/Zapatilla.jpg';
+import Patronaje from '../../assets/imgs/Patron.jpg';
 
 export const  ImgSpinningData = [
     {
-        src: NeoImg,
-        title: 'Modelado de Calzado',
+        src: ZaptillaImg,
+        title: 'Diseno de Calzado',
         description: 'Especialista en Modelaje y Desarrollo Tecnico de Calzado, con dominio integral desde la conceptualizacion de bocetos hasta producccion en serie. Con experincia en patronaje digital(Romance 2D) y diferentes contrucciones, en focado en optimizar la ergonomia mediante el desarrollo preciso de horma y la digitalizacion de procesos para garantiza productos copetitivos y listos para el mercado',
         details: 'Detalles',
     },
     {
-        src: Img1,
-        title: '',
+        src: Patronaje,
+        title: 'Patronaje',
+        description: 'Especialista en Modelaje y Desar digitalizacion de procesos para garantiza productos copetitivos y listos para el mercado',
+        details: 'Detalles',
+    },
+    {
+        src: ZaptillaImg,
+        title: 'Diseno de Calzado',
+        description: 'Especialista en Modelaje y Desarrollo Tecnico de Calzado, con dominio integral desde la conceptualizacion de bocetos hasta producccion en serie. Con experincia en patronaje digital(Romance 2D) y diferentes contrucciones, en focado en optimizar la ergonomia mediante el desarrollo preciso de horma y la digitalizacion de procesos para garantiza productos copetitivos y listos para el mercado',
+        details: 'Detalles',
+    },
+    {
+        src: Patronaje,
+        title: 'Patronaje',
         description: 'Especialista en Modelaje y Desar digitalizacion de procesos para garantiza productos copetitivos y listos para el mercado',
         details: 'Detalles',
     },
