1f37048ac120336284301208d947c68d6499df31
Cefz Zendejas <molanddise@gmail.com>
2026-03-11 22:38:57 -0600

    The HTML, CSS, JS were placed on a single line in the Skills section to optimize the code

--- src/data/moleculesData/SkillsData.js
+++ src/data/moleculesData/SkillsData.js
@@ -1,7 +1,10 @@
 export const SkillsData = [
-  { name: "HTML", preicon: "bx", icon: "bxl-html5", color: "#c95d2e", classes: "html" },
-  { name: "CSS", preicon: "bx", icon: "bxl-css3", color: "#1572b6", classes: "css" },
-  { name: "JavaScript", preicon: "bx", icon: "bxl-javascript", color: "#f7df1e", classes: "javascript" },
+  { name: "HTML  |  CSS  | JS", 
+    preicon: "bx", 
+    icon: ["bxl-html5", "bxl-css3", "bxl-javascript"], 
+    color: ["#c95d2e", "#1572b6", "#f7df1e"], 
+    classes: "html" 
+  },
   { name: "Angular", preicon: "devicon-angular-plain", icon: "colored", color: "#fd7b65bd", classes: "angular" },
   { name: ".Net", preicon: "", icon: "devicon-dot-net-plain-wordmark", color: "rgba(97, 218, 251, 1)", classes: "react" },
 ];
--- src/pages/components/molecules/SkillCard.js
+++ src/pages/components/molecules/SkillCard.js
@@ -1,7 +1,21 @@
 const SkillCard = ({ name, preicon, icon, color, classes }) => {
+     // Convertimos a array si no lo es para que .map() nunca falle
+  const iconsArray = Array.isArray(icon) ? icon : [icon];
+  const colorsArray = Array.isArray(color) ? color : [color];
+
+  const iconsHTML = iconsArray.map((iconName, index) => {
+    // Si hay un color para cada icono lo usa, si no, usa el único color disponible
+    const iconColor = colorsArray[index] || colorsArray[0];
+    
+    return `<i class="${preicon} ${iconName}" style="color:${iconColor}; margin-right: 2px;"></i>`;
+  }).join('');
+
+
   return `
     <div class="bar">
-        <i class="${preicon} ${icon}" style="color:${color}"></i>
+        <div class="icons-container" style="display: flex; align-items: center; margin-bottom: 2px;">
+            ${iconsHTML}
+        </div>
         <div class="info">
             <span>${name}</span>
         </div>
