5c5348d710bacd0fba929675441c99e417cfb47c
Cefz Zendejas <molanddise@gmail.com>
2026-04-12 22:35:14 -0600

    The from was changed to a more structured fromat, and the structures for validating the from submission were added

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -1,7 +1,6 @@
-import { IconsData } from '../data/atomData/IconsData';
-import { IconD } from '../data/atomData/iconD';
-import Icons from '../pages/components/atom/Icons';
-
+import { IconsData } from "../data/atomData/IconsData";
+import { IconD } from "../data/atomData/iconD";
+import Icons from "../pages/components/atom/Icons";
 
 const Footer = () => {
   const view = `
@@ -25,25 +24,82 @@ const Footer = () => {
             </div>
 
             <div class="home__soc-icon">
-                ${IconsData.length > 0 
-                    ? IconsData.map(service => Icons(service)).join('') 
-                    : '<p>No data loaded</p>'
+                ${
+                  IconsData.length > 0
+                    ? IconsData.map((service) => Icons(service)).join("")
+                    : "<p>No data loaded</p>"
                 }
             </div>
         </section>
 
         <section class="contact__form">
-            <form class="form__container" id="contact-form">
-                <input type="text" placeholder="Enter Your Name" required>
-                <span id="error-firstName" class="error-msg"></span>
-                <input type="text" placeholder="Enter Your Last Name" required>
-                <span id="error-firstName" class="error-msg"></span>
-                <input type="email" placeholder="Enter Your Email" required>
-                <span id="error-firstName" class="error-msg"></span>
-                <textarea name="" id="" cols="40" rows="10" placeholder="Enter Your Message"></textarea>
-                <input class="btn-box" type="submit" value="Submit" >
-                <span id="form-status"></span>
+
+            <form class="formulario" id="formulario">
+
+            <div class="formulario__grupo" id="grupo__nombre">
+
+                 <div class="formulario__grupo-input"> 
+                    <input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="">
+                    <label for="nombre" class="formulario__label">Name</label>
+                    <i class="formulario__validacion-estado bx bx-x-circle"></i>
+                 </div>
+                 <span class="formulario__input-error">El nombre tienen que ser de 4 a 16 digitos y solo permite letras</span>
+
+            </div>
+
+             <div class="formulario__grupo" id="grupo__nombre">
+
+                 <div class="formulario__grupo-input"> 
+                    <input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="">
+                    <label for="nombre" class="formulario__label">Enter Your Last Name</label>
+                    <i class="formulario__validacion-estado bx bx-x-circle"></i>
+                 </div>
+                 <span class="formulario__input-error">El nombre tienen que ser de 4 a 16 digitos y solo permite letras</span>
+
+            </div>
+
+            <div class="formulario__grupo" id="grupo__nombre">
+
+                 <div class="formulario__grupo-input"> 
+                    <input type="email" class="formulario__input" name="email" id="mail" placeholder="">
+                    <label for="email" class="formulario__label">Enter Your Email</label>
+                    <i class="formulario__validacion-estado bx bx-x-circle"></i>
+                 </div>
+                 <span class="formulario__input-error">El correo solo debe de contener letras, numeros, puntos, guiones y guion bajo</span>
+
+            </div>
+
+            <div class="formulario__grupo" id="grupo__mensaje">
+                <div class="formulario__grupo-input">
+                    <textarea 
+                    cols="40" 
+                    rows="10"
+                    name="mensaje" 
+                    id="mensaje" 
+                    class="formulario__input" 
+                    placeholder=" "
+                    ></textarea>
+                    <label for="mensaje" class="formulario__label">Enter Your Message</label>
+                    <i class="formulario__validacion-estado bx bx-x-circle"></i>
+                </div>
+                <span class="formulario__input-error">El mensaje debe tener entre 10 y 200 caracteres.</span>
+            </div>
+
+            <div class="formulario__mensaje" id="formulario__mensaje">
+                <p>
+                    <i class='bx bx-error'></i></i><b>Error: </b>Favor de rellenar el formulario
+                    correctamente
+                </p>
+            </div>
+
+
+            <div class="formulario__grupo formulario__grupo-btn-enviar">
+                <button type="submit" class="formulario__btn">Submit</button>
+                <p class="formulario__mensaje-exito" id="formulario__mensaje-exito">Formulario enviado exitosamente</p>
+            </div>
+
             </form>
+
         </section>
 
         <section class="last__text">
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
-import { initForm } from "./src/components/contactForm";
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-document.addEventListener('DOMContentLoaded', () => {
-    initForm();
-});
 
 module.exports = {
   entry: "./src/index.js",
